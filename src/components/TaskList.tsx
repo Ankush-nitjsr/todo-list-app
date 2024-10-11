eb48e0eff24956aa6ac90961f457eb6cadb2544f
Ankush <ankushnitjsr@gmail.com>
2024-10-12 00:34:41 +0530

    feat: Update TaskList component
    Remove filteredTasks from Global state and make local due to infinite re-rendering issue.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,14 +1,13 @@
-import { useTasks } from "../context/TaskContext";
+import { useFilterAndSearchTasks } from "../hooks/useFilterAndSearchTasks";
 import { TaskItem } from "../features/task-item/TaskItem";
 import { Task } from "../types/Task";
 
 export const TaskList = () => {
-  const { filteredTasks } = useTasks();
-  console.log("filtered tasks", filteredTasks);
+  const { filteredTasks } = useFilterAndSearchTasks();
 
   return (
     <div className="tasks-list space-y-4 flex flex-col justify-center items-center w-full">
-      {filteredTasks.length === 0 ? (
+      {filteredTasks === null ? (
         <p>No tasks available</p>
       ) : (
         filteredTasks.map((task: Task) => (
