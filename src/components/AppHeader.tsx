2ff11c5ab92b0a932b22c532dd54455506767e85
Ankush <ankushnitjsr@gmail.com>
2024-10-12 01:03:21 +0530

    chore: Add style

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,11 +3,11 @@ import { SearchTasks } from "../features/search-tasks/SearchTasks";
 
 export const AppHeader = () => {
   return (
-    <div className="flex justify-between w-full my-8">
-      <div className="title">
+    <div className="flex justify-between w-full my-8 items-center">
+      <div className="title font-extrabold text-3xl">
         <h1>Today</h1>
       </div>
-      <div className="search-section">
+      <div className="search-section flex-grow mx-8">
         <SearchTasks />
       </div>
       <div className="filter-section">
