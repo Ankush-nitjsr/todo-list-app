31e882623856526f8c2025ba6bd793c3b580045f
Ankush <ankushnitjsr@gmail.com>
2024-10-12 12:20:31 +0530

    chore: Add style for mobile device view

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,11 +3,11 @@ import { SearchTasks } from "../features/search-tasks/SearchTasks";
 
 export const AppHeader = () => {
   return (
-    <div className="flex justify-between w-full my-8 items-center">
+    <div className="flex flex-col md:flex-row md:justify-between w-full mb-4 md:mb-8 md:items-center space-y-4 md:space-y-0">
       <div className="title font-extrabold text-3xl">
         <h1>Today</h1>
       </div>
-      <div className="search-section flex-grow mx-8">
+      <div className="search-section flex-grow md:mx-8">
         <SearchTasks />
       </div>
       <div className="filter-section">
