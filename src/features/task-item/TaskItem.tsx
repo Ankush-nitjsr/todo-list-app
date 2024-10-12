b716a5337b28ca01f6358fb689f522a23d57c1f0
Ankush <ankushnitjsr@gmail.com>
2024-10-12 12:01:11 +0530

    chore: Add style
    Add style for completed task and organize task item

--- src/features/task-item/TaskItem.tsx
+++ src/features/task-item/TaskItem.tsx
@@ -24,14 +24,23 @@ export const TaskItem = ({ task }: TaskItemProps) => {
   };
 
   return (
-    <div className="flex border border-gray-400 p-2 rounded-lg w-full space-x-2">
-      <CompleteTask task={task} />
-      <TIInnerText
-        task={task}
-        isEditing={isEditing}
-        onUpdateTask={handleUpdateTask} // Call handleUpdateTask to save and exit edit mode
-      />
-      <div className="flex space-x-2">
+    <div
+      className={`flex justify-between items-center border border-gray-400 px-4 py-2 rounded-lg w-full space-x-2 ${
+        task.completed ? "bg-green-200" : ""
+      }`} // Apply green background if completed
+    >
+      {/* Left section with CompleteTask and TIInnerText */}
+      <div className="flex items-center space-x-3">
+        <CompleteTask task={task} />
+        <TIInnerText
+          task={task}
+          isEditing={isEditing}
+          onUpdateTask={handleUpdateTask}
+        />
+      </div>
+
+      {/* Right section with EditTask and DeleteTask */}
+      <div className="flex items-center space-x-2">
         <EditTask
           task={task}
           isEditing={isEditing}
