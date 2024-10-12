3bd6e699fa81880e0759df27e385308664f30bb1
Ankush <ankushnitjsr@gmail.com>
2024-10-12 12:31:07 +0530

    chore: Add icon for delete task

--- src/features/delete-task/DeleteTask.tsx
+++ src/features/delete-task/DeleteTask.tsx
@@ -1,3 +1,4 @@
+import { TrashIcon } from "@heroicons/react/24/outline";
 import { Task } from "../../types/Task";
 import { useDeleteTask } from "./useDeleteTask";
 
@@ -7,5 +8,9 @@ interface DeleteTaskProps {
 
 export const DeleteTask = ({ task }: DeleteTaskProps) => {
   const { handleDeleteTask } = useDeleteTask();
-  return <button onClick={() => handleDeleteTask(task.id)}>x</button>;
+  return (
+    <button onClick={() => handleDeleteTask(task.id)}>
+      <TrashIcon className="h-5 w-5 text-red-600" />
+    </button>
+  );
 };
